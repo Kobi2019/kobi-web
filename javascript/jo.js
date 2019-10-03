1da2dad361fbabfae140833f2dea7b070585d107
Kobi2019 <kobynappier18@gmail.com>
2019-10-03 21:55:49 +0100

    my projects

---
+++ javascript/jo.js
@@ -0,0 +1,78 @@
+Document.getElementById("demo")
+.innerHTML="this is a boy";
+
+function  change(){
+  document.getElementById("demo")
+  .innerHTML="i am a boy";
+}
+
+function numbers(){
+  var x = 3;
+  var y = 2;
+  var z = x + y;
+  document.write(z);
+}
+ typeof "koby"
+
+//output
+window.alert() //alert
+
+document.write() //html Document
+
+console.log() //console output for testing
+
+innerHTML  //to write into an html element
+
+
+
+//string
+var str ="boy";
+//number
+var nu =23;
+
+//function
+function name(){
+
+}
+
+//object
+var obj ={
+  fistName:"koby",
+  surName:"nappier",
+  age: 19 ,
+  eyeColor: "black",
+}
+
+//arrey
+
+var arr =[
+  "kate", "john", "sam"
+];
+
+//booleans
+
+var x = true;
+var y = false;
+
+//empty values
+var car = ""; //null
+
+var soa ;//undefined
+
+.
+//operators
+// -,%, / ,+ ,++ ,-,--, *
+
+//logical operators
+// == equal t0
+// === equal value and equal type
+// !== not equal or not equal values()
+
+
+function myFunction(parameter){
+  var argu ="arguments";
+
+return parameter;
+// or
+return (parameter)
+}
